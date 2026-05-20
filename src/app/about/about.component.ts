38043089e85c12760f0bda9b7abc83a1ca2dbfd3
Elizaveta B <anthemion@yandex.ru>
2026-05-20 21:01:30 +0100

    Test concatMap operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { concat, of } from "rxjs";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css'],
-    standalone: false
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
+  standalone: false,
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const sourse1$ = of(1, 2, 3);
+    const sourse2$ = of(4, 5, 6);
+    const sourse3$ = of(7, 8, 9);
 
+    const result$ = concat(sourse1$, sourse2$, sourse3$);
+
+    result$.subscribe((x) => console.log(x));
+  }
 }
